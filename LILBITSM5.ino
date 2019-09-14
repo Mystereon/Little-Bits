50228f9adb514991a99da063b745013a3330d7d9
Ian Parker <venomstreak@gmail.com>
2019-09-15 00:20:03 +0100

    Add files via upload

--- LILBITSM5.ino
+++ LILBITSM5.ino
@@ -1,7 +1,7 @@
 //The Game of Life, also known simply as Life, is a cellular automaton
 //devised by the British mathematician John Horton Conway in 1970.
 // https://en.wikipedia.org/wiki/Conway's_Game_of_Life
-#include <FastLED.h>
+
 #include <M5StickC.h>
 
 //ULTLORES
@@ -19,7 +19,7 @@ int viewy = 80;
 #define GRIDX 120
 #define GRIDY 80
 #define CELLXY 1
-CRGB leds[8];
+
 #define TFT_GREY 0x1010
 #define TFT_ORANGE 0xFFA5
 int runt = 0;
@@ -35,10 +35,9 @@ int gens = NUMGEN;
 uint16_t genCount = 0;
 
 void setup()   {
-  FastLED.addLeds<WS2812B, 26, GRB>(leds, 8);
-  LEDS.setBrightness(50);
+
   pinMode(M5_LED, OUTPUT);
-  pinMode(G26, OUTPUT);
+
   digitalWrite(M5_LED, LOW);
 
   //Set up the display
@@ -83,72 +82,71 @@ double vbat = 0.0;
 int discharge, charge;
 double temp = 0.0;
 double bat_p = 0.0;
-void loop() {
-
- 
-  int GEN_DELAY = analogRead(G0) / 200;
-  //Display a simple splash screen
-
-
-  M5.Lcd.fillScreen(random(0xFFFF));
-
-  initGrid();
-
-  genCount = NUMGEN;
-
-  drawGrid();
-
-  //Compute generations
-  for (int gen = 0; gen < gens; gen++)
-  {
-    if (digitalRead(M5_BUTTON_HOME) == LOW) {
-      initGrid(); gen = 0; M5.Lcd.setTextSize(2); M5.Lcd.fillScreen(TFT_BLACK); M5.Lcd.setTextColor(TFT_WHITE, TFT_GREY); M5.Lcd.setCursor(35, 27);
-      M5.Lcd.println(F("  NUKE  ")); delay(GEN_DELAY * random(20)); rcolor = random(0xFFFF); M5.Lcd.fillScreen(random(0xFFFF)); runt--; int GEN_DELAY = analogRead(G0) / 200;
-    }
-    if (digitalRead(M5_BUTTON_RST) == LOW) {
-      setres();
-      M5.Lcd.setRotation(3);
-    }
-    vbat      = M5.Axp.GetVbatData() * 1.1 / 1000;
-    M5.Lcd.fillRect(120, 0, 120, 80, BLACK);
-    M5.Lcd.setTextColor(TFT_WHITE, BLACK);
-    M5.Lcd.setCursor(122, 00);
-    M5.Lcd.setTextSize(2);
-    M5.Lcd.println("GEN");
-    M5.Lcd.setCursor(122, 20);
-    M5.Lcd.setTextSize(2);
-    M5.Lcd.println(gen);
-    M5.Lcd.setCursor(122, 40);
-    M5.Lcd.setTextSize(2);
-    M5.Lcd.println("RUN");
-    M5.Lcd.setCursor(122, 60);
-    M5.Lcd.setTextSize(2);
-    M5.Lcd.println(runt);
-    M5.Lcd.setCursor(0, 70);
-    M5.Lcd.setTextSize(1);
-    M5.Lcd.setTextColor(BLACK, RED);
-    M5.Lcd.printf("%.3fV\r\n", vbat);
-    if (vbat <= 3.8 ) {
-      M5.Lcd.setCursor(20, 30);
-      M5.Lcd.setTextSize(3);
-      M5.Lcd.printf("FEED ME");
-      LEDS.setBrightness(10);
-      WS2812PLAY();
-    }
-    digitalWrite(M5_LED, HIGH);
-    delay(GEN_DELAY);
-
-    computeCA();
-    drawGrid();
-
-    for (int16_t x = 1; x < viewx - 1; x++) {
-      for (int16_t y = 1; y < viewy - 1; y++) {
-        grid[x][y] = newgrid[x][y];
+      void loop() {
+      
+       
+        int GEN_DELAY = analogRead(G0) / 400;
+        //Display a simple splash screen
+      
+      
+       
+        initGrid();
+      
+        genCount = NUMGEN;
+      
+        drawGrid();
+      
+        //Compute generations
+        for (int gen = 0; gen < gens; gen++)
+        {
+          if (digitalRead(M5_BUTTON_HOME) == LOW) {
+             gen = 0; M5.Lcd.setTextSize(2);  M5.Lcd.setTextColor(TFT_WHITE, RED); M5.Lcd.setCursor(35, 27);
+            M5.Lcd.println(F("  NUKE  ")); delay(200); rcolor = random(0xFFFF);runt--;initGrid(); int GEN_DELAY =  20;
+          }
+          if (digitalRead(M5_BUTTON_RST) == LOW) {
+            setres();
+            M5.Lcd.setRotation(3);
+          }
+          vbat      = M5.Axp.GetVbatData() * 1.1 / 1000;
+          
+          M5.Lcd.setTextColor(TFT_WHITE, BLACK);
+          M5.Lcd.setCursor(132, 5);
+          M5.Lcd.setTextSize(1);
+          M5.Lcd.println("GEN");
+          M5.Lcd.setCursor(122, 20);
+          M5.Lcd.setTextSize(2);
+          M5.Lcd.println(gen);
+          M5.Lcd.setCursor(132, 45);
+          M5.Lcd.setTextSize(1);
+          M5.Lcd.println("RUN");
+          M5.Lcd.setCursor(122, 60);
+          M5.Lcd.setTextSize(2);
+          M5.Lcd.println(runt);
+          M5.Lcd.setCursor(0, 70);
+          M5.Lcd.setTextSize(1);
+          M5.Lcd.setTextColor(BLACK, RED);
+          M5.Lcd.printf("%.3fV\r\n", vbat);
+          if (vbat <= 3.8 ) {
+            M5.Lcd.setCursor(20, 30);
+            M5.Lcd.setTextSize(3);
+            M5.Lcd.printf("FEED ME");
+            
+            
+          }
+          digitalWrite(M5_LED, HIGH);
+          delay(GEN_DELAY);
+      
+          computeCA();
+          drawGrid();
+      
+          for (int16_t x = 1; x < viewx - 1; x++) {
+            for (int16_t y = 1; y < viewy - 1; y++) {
+              grid[x][y] = newgrid[x][y];
+            }
+          }
+      
+        }
       }
-    }
-
-  }
-}
 
 //Draws the grid on the display
 void drawGrid(void) {
@@ -168,7 +166,8 @@ int color = 0x0000;
 //Initialise Grid
 void initGrid(void) {
   M5.Lcd.setRotation(3);
-  
+   M5.Lcd.fillScreen(BLACK);
+M5.Lcd.drawRect(0, 0, 120, 80, random(0xFFFF));
   for (int16_t x = 0; x < viewx; x++) {
     for (int16_t y = 0; y < viewy; y++) {
       newgrid[x][y] = 0;
@@ -186,8 +185,8 @@ void initGrid(void) {
     }
   }
   runt++;
-  WS2812PLAYG();
-  rcolor == random(0xFFFF);
+  
+  
 }
 
 //Compute the CA. Basically everything related to CA starts here
@@ -229,7 +228,7 @@ void statusSys() {
   while (digitalRead(M5_BUTTON_HOME) == HIGH) {
 
 
-    WS2812PLAY();
+    
 
     vbat      = M5.Axp.GetVbatData() * 1.1 / 1000;
     temp      =  -144.7 + M5.Axp.GetTempData() * 0.1;
@@ -247,40 +246,7 @@ void statusSys() {
   setres();
 }
 
-void WS2812PLAY() {
-  // Move a single white led
-  for (int whiteLed = 0; whiteLed < 8; whiteLed = whiteLed + 1) {
-    // Turn our current led on to white, then show the leds
-    leds[whiteLed] = CRGB::Red;
-    leds[8 - whiteLed] = CRGB::Red;
 
-    // Show the leds (only one of which is set to white, from above)
-    FastLED.show();
-    delay(100);
-
-    // Turn our current led back to black for the next loop around
-    leds[whiteLed] = CRGB::Black;
-    leds[8 - whiteLed] = CRGB::Black;
-  }
-}
-
-void WS2812PLAYG() {
-  // Move a single white led
-  for (int whiteLed = 0; whiteLed < 8; whiteLed = whiteLed + 1) {
-    // Turn our current led on to white, then show the leds
-    leds[whiteLed] = CRGB::Green;
-    leds[8 - whiteLed] = CRGB::Blue;
-    // Show the leds (only one of which is set to white, from above)
-    FastLED.show();
-
-    // Wait a little bit
-    delay(100);
-
-    // Turn our current led back to black for the next loop around
-    leds[whiteLed] = CRGB::Black;
-    leds[8 - whiteLed] = CRGB::Black;
-  }
-}
 // menu select resolution
 void setres(){                           extern const unsigned char gImage_002[];
                                          M5.Lcd.setRotation(0);
@@ -300,12 +266,11 @@ void setres(){                           extern const unsigned char gImage_002[]
                                         if (res == 5) { res = 1;}
                                         CELLXY == res;
                                         if (res ==1) { viewx = 120; viewy = 80;gens = 999;}
-                                        if (res ==2) { viewx = 60; viewy = 40;gens = 750;}
-                                        if (res ==3) { viewx = 40; viewy = 30; gens = 500;}
-                                        if (res ==4) { viewx = 30; viewy = 20; gens = 300;}
+                                        if (res ==2) { viewx = 60; viewy = 40;gens = 999;}
+                                        if (res ==3) { viewx = 40; viewy = 30; gens = 800;}
+                                        if (res ==4) { viewx = 30; viewy = 20; gens = 500;}
                                         delay(50);
-                                       void WS2812PLAY();
+                                       
                                         }
-                                        M5.Lcd.setRotation(0);
-                                        M5.Lcd.drawBitmap(0, 0, 80, 160, (uint16_t *)gImage_002);delay(1000);
+                                        M5.Lcd.fillScreen(BLACK);
 }
